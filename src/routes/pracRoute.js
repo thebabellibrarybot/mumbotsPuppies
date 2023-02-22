91a384174220c7017f3f270c4eb0e4e5a943b190
thebabellibrarybot <jack@thebabellibrary.org>
2023-02-21 23:48:00 -0500

    eod home routes enabled w/thunderclient

--- src/routes/pracRoute.js
+++ src/routes/pracRoute.js
@@ -1,10 +1,16 @@
-const fetchTitleHref = require('../controllers/pup2');
+const { fetchTitleHref } = require('../controllers/pup2');
 
 
+router.get('/', fetchTitleHref)
+
+/*
 const url = 'http://ica.themorgan.org/manuscript/thumbs/77146';
+const className = '.col-xs-12.col-sm-2 a';
+const elType = 'href';
 
-async function run(url) {
-    const data = await fetchTitleHref(url);
+async function run(url, className, elType) {
+    const data = await fetchTitleHref(url, className, elType);
     console.log(data);
-  }
-run(url);
+  };
+run(url, className, elType); 
+*/

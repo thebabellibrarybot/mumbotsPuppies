3d36519cdf931587be36622fad49f11ef901865f
thebabellibrarybot <jack@thebabel.org>
2023-02-23 18:29:40 -0500

    mk update routes

--- src/utils/findTombsByDate.js
+++ src/utils/findTombsByDate.js
@@ -11,7 +11,7 @@ const date = '12146';
 async function findUrls (date, url) {
 
     try {
-
+        // return obj
         const urlArray = [];
 
         // start puppeteer
@@ -29,34 +29,25 @@ async function findUrls (date, url) {
         await page.waitForNavigation();
 
         // save page url and look for next button
+        let currentPageUrl = page.url();
+        urlArray.push(currentPageUrl);
+
+        // click to next page and save url
         while (true) {
-            // Get the URL of the current page
-            const currentPageUrl = page.url();
-            urlArray.push(currentPageUrl); // save the URL to the array
-        
-            // Check if a next button is present
-            const links = await page.$$('a');
-            const nextLink = await links.find(async link => {
-                const title = await link.getProperty('title').then(prop => prop.jsonValue());
-                return title;
-            });
-            console.log(nextLink, 'next link');
-            if (!nextLink) {
-                console.log('no next')
-                break; // exit the loop if there is no next button
-            };
-            // click the next button and wait for the page to load
-            if (nextLink) {
-                await page.click(nextLink);
-                await page.waitForNavigation();
-            };
+
+            const nextButton = await page.$('a[title="Go to next page"]');
+            if (!nextButton) {
+                break;
+            }
+            await nextButton.click();
+            await page.waitForNavigation();
+            currentPageUrl = page.url();
+            urlArray.push(currentPageUrl);
           };
 
-        //console.log(urlArray);
-        //await browser.close();
+        await browser.close();
 
-
-        return urlArray;
+        return urlArray
 
     } catch (err) {
         console.log(err);
@@ -65,8 +56,7 @@ async function findUrls (date, url) {
 
 };
 
-// takes date and base url parameters (str, str)
-// returns an dict with all tombs listed under that date {"year": ["tomb_name": "href"]}
+/* CAN DELET FOR REF
 async function findAllTombsUrls (urlArray) {
 
     console.log(date, url, 'date and baseurl')
@@ -115,7 +105,60 @@ async function findAllTombsUrls (urlArray) {
     }
 
 };
+*/
+
+// takes array of urls
+// returns data from chart in each url
+async function findUrlCharts (date, url) {
+
+    const urlArray = await findUrls(date, url);
+
+    try {
+        const resObj = {};
+        // set up cluster to handle multiple links
+        const cluster = await Cluster.launch({
+            concurrency: Cluster.CONCURRENCY_CONTEXT,
+            maxConcurrency: 2,
+            puppeteerOptions: {
+                headless: false,
+                defaultViewport: false,
+                userDataDir: "./tmp"
+            }
+            });
+        // start task-master: task = goto url and scrap data
+        await cluster.task(async ({ page, data: url }) => {
+            await page.goto(url);
+            // scrape all tomb on page
+            const rows = await page.$$('tbody tr');
+            for (const row of rows) {
+                const rowData = [];
+                // Process each row here
+                const cells = await row.$$('td');
+                for (const cell of cells) {
+                    const text = await cell.evaluate(el => el.textContent);
+                    rowData.push(text);
+                };
+                resObj[rowData[0].trim()] = {"type": rowData[1].trim(), "location": rowData[2].trim(), "date": rowData[3].trim()}                
+            };
+        });
+        
+        // load array of urls
+        for (const url of urlArray) {
+            await cluster.queue(url);
+            }            
+        await cluster.idle();
+        await cluster.close();
+        return resObj;
+    } catch (err) {
+        console.log(err, 'from findUrlCharts');
+    };
+};
 
 
   
-findUrls(date, baseurl).then(result => console.log(result, 'res')).catch(error => console.error(error));
+//const i = findUrls(date, baseurl).then(result => console.log(result, 'res')).catch(error => console.error(error));
+const urlss = [
+    'https://www.themorgan.org/manuscripts/list?field_medieval_centuries_tid=12146&field_image_available_tid=All&field_ms_type_tid=All&field_country_taxonomy_tid=All&items_per_page=400',
+    'https://www.themorgan.org/manuscripts/list?field_medieval_centuries_tid=12146&field_image_available_tid=All&field_ms_type_tid=All&field_country_taxonomy_tid=All&items_per_page=400&page=1'
+  ];
+findUrlCharts(date, baseurl).then(result => console.log(result, 'resII')).catch(error => console.error(error));

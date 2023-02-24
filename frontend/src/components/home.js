ae21f97bd9629da9c60a51cc8f593528d2085ccf
thebabellibrarybot <jack@thebabellibrary.com>
2023-02-24 14:42:05 -0500

    added client

---
+++ frontend/src/components/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BarChart from '../pgComponents/barChart';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <BarChart></BarChart>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ frontend/src/pgComponents/barChart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BarChart extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, this is BarChart</h1>
+      </div>
+    );
+  }
+}
+
+export default BarChart;
